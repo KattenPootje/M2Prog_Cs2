234cce05b8db296c186d1f112282cd821eee70fc
Ilya Stroomer <39950@ma-web.nl>
2025-11-18 12:51:44 +0100

    hallo

--- Functions/Program.cs
+++ Functions/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Program program = new Program(); //hallo
+        program.Run();
+    }
+    internal void Run()
+    {
+        Console.WriteLine("Dit is nu de start van mijn programma");
+        return;
     }
 }
